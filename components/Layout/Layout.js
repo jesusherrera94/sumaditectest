c7e47a3010292568a27b4dfa9570bc8264ae51f8
Jesus Herrera <jesusherrera1594@gmail.com>
2020-12-08 12:26:06 -0600

    Network info added

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -2,11 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React, {useState,useEffect} from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import * as Battery from 'expo-battery';
+import NetInfo from '@react-native-community/netinfo';
 
 const Layout = ({children,navigation}) => {
 
     const [batteryLevel, setBatteryLevel] = useState(null);
-
+    const [networkInfo, setNetworkInfo] = useState({
+                                                    type:null,
+                                                    isConnected:null
+                                                    });
     //Battery level
      const batterySubscribe = async () => {
         const batteryLevel = await Battery.getBatteryLevelAsync();
@@ -14,13 +18,22 @@ const Layout = ({children,navigation}) => {
       }
 
       useEffect(()=>{
+          //battery info
           batterySubscribe();
           const subs = Battery.addBatteryLevelListener(({ batteryLevel }) => {
                             setBatteryLevel(batteryLevel);
                         });
+          //Netinfo
+          const unsubscribe = NetInfo.addEventListener(state => {
+            setNetworkInfo({
+                            type:state.type,
+                            isConnected: state.isInternetReachable?"Conected":"No internet"
+                            });
+          });               
           //unmount
           return () =>{
               subs.remove();
+              unsubscribe();
           }
       },[])
 
@@ -28,7 +41,7 @@ const Layout = ({children,navigation}) => {
     <View style={styles.container}>
         <View>
             <Text>Battery: {Math.round(batteryLevel*100)}%</Text>
-            <Text>Internet: </Text>
+            <Text>Conection: {networkInfo.type} {networkInfo.isConnected}</Text>
         </View>
       <Text>Layout</Text>
       {children}
